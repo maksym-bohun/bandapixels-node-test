ce7d880b196a79e737751b3d2df025b60ffc46d9
Maksym-Bohun <maks.boggun@gmail.com>
2024-07-02 00:45:58 +0300

    feat: Created client and server sides

---
+++ client/src/components/ItemCard.component.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const ItemCard = () => {
+  return <Container></Container>;
+};
+
+export default ItemCard;
+
+const Container = styled.div``;
